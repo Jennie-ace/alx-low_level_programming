f5352f13bf91d102a64be05f65bfead0e4c52215
Jennie-ace <maxwelljennifer369@gmail.com>
2022-09-15 15:16:53 -0700

    A program that prints a square of input size

---
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * print_square - prints a square of input size
+ * @size: dimensions of square
+ * Return: nothing
+ */
+
+void print_square(int size)
+{
+	int x, y;
+
+	for (y = 0; y < size; y++)
+	{
+		for (x = 0; x < size; x++)
+			_putchar('#');
+		_putchar('\n');
+	}
+	if (size < 1)
+		_putchar('\n');
+}
+
