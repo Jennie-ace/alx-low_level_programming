aea7481efda361e4504072090ef7f906f6dc906c
Jennie-ace <maxwelljennifer369@gmail.com>
2022-09-28 16:03:33 -0700

    C recursion

--- 0x08-recursion/101-wildcmp.c
+++ 0x08-recursion/101-wildcmp.c
@@ -57,7 +57,7 @@ char *postfix_match(char *str, char *postfix)
 	int str_len = strlen_no_wilds(str) - 1;
 	int postfix_len = strlen_no_wilds(postfix) - 1;
 
-if (*postfix == '*')
+	if (*postfix == '*')
 	iterate_wild(&postfix);
 
 	if (*(str + str_len - postfix_len) == *postfix && *postfix != '\0')
