6417954cf3570e3283b082d3a8a8f6cdf3c23793
Jennie-ace <maxwelljennifer369@gmail.com>
2022-10-03 12:49:38 -0700

    Prints number of arguments passed

---
+++ 0x0A-argc_argv/2-args.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - print number of arguments passed to main
+ * @argc: number of actual parameter passed to main
+ * @argv: pointer to the first element passed to main
+ *
+ * Return: Success
+ */
+int main(int argc, char *argv[])
+{
+	int count = 0;
+
+	while (argc--)
+	{
+		printf("%s\n", argv[count]);
+		count++;
+	}
+	exit(EXIT_SUCCESS);
+}
